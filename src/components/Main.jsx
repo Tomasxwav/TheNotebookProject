c734ad3e2ccd57a36573443c51f08e31a8c16a0c
Tomasxwav <tomas_everardo9@hotmail.com>
2024-07-17 00:21:33 -0600

    Notas por medio de realtime database firebase

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,68 +1,56 @@
 import PreviewNote from './PreviewNote.jsx'
-import notes from "../mocks/database.json"
 
 import app from '../database/connection.js';
 import { getDatabase, ref, get, child } from "firebase/database";
+import { useEffect, useState } from 'react';
 
-const json = [
-    {
-      "color": '#FECECE',
-      "titulo": "Mi Primer JSON",
-      "contenido": "Este es el contenido de ejemplo de mi primer JSON.",
-      "fecha": "2024-06-28"
-    },
-    {
-      "color": "#CED2FE",
-      "titulo": "Segundo Ejemplo",
-      "contenido": "Este es el contenido del segundo ejemplo.",
-      "fecha": "2024-06-29"
-    },
-    {
-      "color": "#CEFEDE",
-      "titulo": "Tercer Ejemplo",
-      "contenido": "Aquí va el contenido del tercer ejemplo.",
-      "fecha": "2024-06-30"
-    },
-    {
-      "color": "#FDFECE",
-      "titulo": "Cuarto Ejemplo",
-      "contenido": "Contenido del cuarto ejemplo.",
-      "fecha": "2024-07-01"
-    },
-    {
-      "color": "#FACEFE",
-      "titulo": "Quinto Ejemplo",
-      "contenido": "Este es el contenido del quinto ejemplo.",
-      "fecha": "2024-07-02"
-    },
-    {
-      "color": "#F7FECE",
-      "titulo": "Sexto Ejemplo",
-      "contenido": "Contenido del sexto ejemplo.",
-      "fecha": "2024-07-03"
-    }
-  ]
+
 const db = getDatabase();
 const users = ref(db);
+
 
 let username = "tomas";
 let folder = "folder1";
 
-get(child(users, 'users/' + username + '/folders:/' + folder)).then((notes) => {
-  console.log(notes.val());
-  const mynotes = notes.forEach((e) => { console.log(e.val())})
-}).catch((error) => {
-  console.error(error);
-});
+
+
 
 export function Main() {
+  const [allNotes, setAllNotes] = useState([])
+
+  useEffect(() => {
+    let count = 0;
+    let temp = []
+    get(child(users, 'users/' + username + '/folders/'))
+        .then(folders => {
   
+          folders.forEach(folder => {
+  
+            folder.forEach(notes => {
+  
+              notes.forEach((note) => {
+                
+                count ++;
+                // console.log(allNotes);
+                temp.push(note.val());
+                setAllNotes(temp)
+              })
+  
+            })
+  
+  
+          })
+  
+        })
+  } ,[])
+
   return (
       <>
-      {json.map((note, index) => (
-          <PreviewNote key={index} stickyColor={note.color} title={note.titulo}  content={note.contenido} date={note.fecha}/>
+      {allNotes.map((note, index) => (
+        note && <PreviewNote key={index} stickyColor={note.color} title={note.title}  content={note.content} date={note.date}/>
+        // <h1 key={index}>{note.title}</h1>
       ))}
-      <PreviewNote title="caca"/>
+      
       
       </>
   )
